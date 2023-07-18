4fa5a07a2ec2615f48138dc4a490e309e51fb271
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-07-18 12:53:57 +0300

    Initial + design

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Aside } from './components/aside/Aside';
+import { Header } from './components/header/Header';
+import { Main } from './components/main/Main';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+      <Aside />
+    </div>
+  );
+}
+
+export default App;
